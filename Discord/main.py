fa30497fbea1ac923d7c648683a5b73b06ba2bc2
aobBlindFish <a.pmg.contact@gmail.com>
2021-04-06 22:58:23 +0200

    add Discord connection

---
+++ Discord/main.py
@@ -0,0 +1,23 @@
+import discord
+import os
+from stay_awake import keep_alive
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print("we have logged in as {0.user}".format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello")
+
+
+keep_alive()
+client.run(os.getenv("TOKEN"))
